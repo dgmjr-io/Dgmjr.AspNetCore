cbca98257951bbe23b2a8e9e1f6935c5c2b3b5d4
David G. Moore, Jr. <david@dgmjr.io>
2023-12-29 00:34:10 -0500

    added suffixes to the mime project

--- src/Formatters/FormattersExtensions.cs
+++ src/Formatters/FormattersExtensions.cs
@@ -7,6 +7,7 @@ using MessagePack.AspNetCoreMvcFormatter;
 using MessagePack.Resolvers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
 using Microsoft.Net.Http.Headers;
 
 #if NET5_0_OR_GREATER
@@ -17,97 +18,136 @@ public static class FormattersExtensions
 {
     public static IServiceCollection AddFormatters(this IServiceCollection services)
     {
-        services
-            .AddControllers(options =>
-            {
-                options.RespectBrowserAcceptHeader = true;
-                options.ReturnHttpNotAcceptable = true;
+#if NET5_0_OR_GREATER
+        services.AddControllers();
+#endif
+        services.Configure<MvcOptions>(options =>
+        {
+            options.RespectBrowserAcceptHeader = true;
+            options.ReturnHttpNotAcceptable = true;
 
-                options.OutputFormatters.Add(
-                    new Dgmjr.Payloads.Formatters.PlainTextPayloadFormatter()
-                );
-                options.InputFormatters.Add(new PlainTextInputFormatter());
-                options.OutputFormatters.Add(new BsonOutputFormatter());
-                options.OutputFormatters.Add(new PlainTextProblemDetailsOutputFormatter());
-                options.OutputFormatters.Add(new PlainTextOutputFormatter());
+            // options.OutputFormatters.Add(
+            //     new Dgmjr.Payloads.Formatters.PlainTextPayloadFormatter()
+            // );
+            options.InputFormatters.Add(new PlainTextInputFormatter());
+            options.OutputFormatters.Add(new BsonOutputFormatter());
+            options.OutputFormatters.Add(new PlainTextProblemDetailsOutputFormatter());
+            options.OutputFormatters.Add(new PlainTextOutputFormatter());
 
-                options.OutputFormatters.Add(new MessagePackOutputFormatter(new MessagePackSerializerOptions(ContractlessStandardResolver.Instance)));
-                options.InputFormatters.Add(new MessagePackInputFormatter(new MessagePackSerializerOptions(ContractlessStandardResolver.Instance)));
+            options.OutputFormatters.Add(
+                new MessagePackOutputFormatter(
+                    new MessagePackSerializerOptions(ContractlessStandardResolver.Instance)
+                )
+                {
+                    SupportedMediaTypes =
+                    {
+                        Application.MessagePack.Synonyms
+                        new MediaTypeHeaderValue("application/x-msgpack"),
+                        new MediaTypeHeaderValue("application/x-msgpack-seq")
+                    }
+                }
+            );
+            options.InputFormatters.Add(
+                new MessagePackInputFormatter(
+                    new MessagePackSerializerOptions(ContractlessStandardResolver.Instance)
+                )
+            );
 
-                options.InputFormatters.Add(new XmlSerializerInputFormatter());
-                options.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
-            })
-            .AddXmlSerializerFormatters()
-            .AddXmlDataContractSerializerFormatters()
-            .Configure<MvcOptions>(options =>
-            {
-                // Since both XmlSerializer and DataContractSerializer based formatters
-                // have supported media types of 'application/xml' and 'text/xml',  it
-                // would be difficult for a test to choose a particular formatter based on
-                // request information (Ex: Accept header).
-                // We'll configure the ones on MvcOptions to use a distinct set of content types.
+            // Since both XmlSerializer and DataContractSerializer based formatters
+            // have supported media types of 'application/xml' and 'text/xml',  it
+            // would be difficult for a test to choose a particular formatter based on
+            // request information (Ex: Accept header).
+            // We'll configure the ones on MvcOptions to use a distinct set of content types.
+            options.InputFormatters.Add(
+                new XmlSerializerInputFormatter(options)
+                {
+                    SupportedMediaTypes =
+                    {
+                        "application/xmlser+xml",
+                        Application.Any.DisplayName + "+xml",
+                        Application.ProblemXml.DisplayName,
+                        Text.Any.DisplayName + "+xml",
+                        Application.Xml.DisplayName,
+                        Text.Xml.DisplayName
+                    }
+                }
+            );
+            options.InputFormatters.Add(
+                new XmlDataContractSerializerInputFormatter(options)
+                {
+                    SupportedMediaTypes =
+                    {
+                        "application/dcs+xml",
+                        "application/xml-dcs",
+                        Application.Any.DisplayName + "+xml",
+                        Application.ProblemXml.DisplayName,
+                        Text.Any.DisplayName + "+xml",
+                        Application.Xml.DisplayName,
+                        Text.Xml.DisplayName
+                    }
+                }
+            );
 
-                var xmlSerializerInputFormatter = options.GetXmlSerializerInputFormatter();
-                var xmlSerializerOutputFormatter = options.GetXmlSerializerOutputFormatter();
-                var dcsInputFormatter = options.GetXmlDataContractSerializerInputFormatter();
-                var dcsOutputFormatter = options.GetXmlDataContractSerializerOutputFormatter();
-
-                options.InputFormatters.RemoveRange([xmlSerializerInputFormatter, dcsInputFormatter]);
-                options.OutputFormatters.RemoveRange([xmlSerializerOutputFormatter, dcsOutputFormatter]);
-
-                xmlSerializerInputFormatter.SupportedMediaTypes.Clear();
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("application/*+xml");
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("application/problem+xml");
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("application/xml");
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("application/xmlser+xml");
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("text/*+xml");
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("text/xml");
-                xmlSerializerInputFormatter.SupportedMediaTypes.Add("text/xmlser+xml");
-
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Clear();
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("application/*+xml");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("application/problem+xml");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("application/xml");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("application/xmlser+xml");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("text/*+xml");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("text/problem+plain");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("text/xml");
-                xmlSerializerOutputFormatter.SupportedMediaTypes.Add("text/xmlser+xml");
-
-                dcsInputFormatter.SupportedMediaTypes.Clear();
-                dcsInputFormatter.SupportedMediaTypes.Add("application/dcs+xml");
-                dcsInputFormatter.SupportedMediaTypes.Add("application/problem+xml");
-                dcsInputFormatter.SupportedMediaTypes.Add("application/xml");
-                dcsInputFormatter.SupportedMediaTypes.Add("text/dcs+xml");
-                dcsInputFormatter.SupportedMediaTypes.Add("text/xml");
-
-                dcsOutputFormatter.SupportedMediaTypes.Clear();
-                dcsOutputFormatter.SupportedMediaTypes.Add("application/dcs+xml");
-                dcsOutputFormatter.SupportedMediaTypes.Add("application/problem+xml");
-                dcsOutputFormatter.SupportedMediaTypes.Add("application/xml-dcs");
-                dcsOutputFormatter.SupportedMediaTypes.Add("text/dcs+xml");
-                dcsOutputFormatter.SupportedMediaTypes.Add("text/xml-dcs");
-
-                options.InputFormatters.Add(dcsInputFormatter);
-                options.InputFormatters.Add(xmlSerializerInputFormatter);
-                options.OutputFormatters.Add(dcsOutputFormatter);
-                options.OutputFormatters.Add(xmlSerializerOutputFormatter);
-            });
+            options.OutputFormatters.Add(
+                new XmlSerializerOutputFormatter
+                {
+                    SupportedMediaTypes =
+                    {
+                        "application/xmlser+xml",
+                        Application.Any.DisplayName + "+xml",
+                        Application.ProblemXml.DisplayName,
+                        Text.Any.DisplayName + "+xml",
+                        Application.Xml.DisplayName,
+                        Text.Xml.DisplayName
+                    }
+                }
+            );
+            options.OutputFormatters.Add(
+                new XmlDataContractSerializerOutputFormatter
+                {
+                    SupportedMediaTypes =
+                    {
+                        "application/dcs+xml",
+                        "application/xml-dcs",
+                        Application.Any.DisplayName + "+xml",
+                        Application.ProblemXml.DisplayName,
+                        Text.Any.DisplayName + "+xml",
+                        Application.Xml.DisplayName,
+                        Text.Xml.DisplayName
+                    }
+                }
+            );
+        });
         return services;
     }
 
-    public static XmlSerializerInputFormatter? GetXmlSerializerInputFormatter(this MvcOptions mvcOptions)
-    => mvcOptions.InputFormatters.FirstOrDefault(formatter => formatter is XmlSerializerInputFormatter) as XmlSerializerInputFormatter;
+    public static XmlSerializerInputFormatter? GetXmlSerializerInputFormatter(
+        this MvcOptions mvcOptions
+    ) =>
+        mvcOptions.InputFormatters.FirstOrDefault(
+            formatter => formatter is XmlSerializerInputFormatter
+        ) as XmlSerializerInputFormatter;
 
-    public static XmlSerializerOutputFormatter? GetXmlSerializerOutputFormatter(this MvcOptions mvcOptions)
-    => mvcOptions.OutputFormatters.FirstOrDefault(formatter => formatter is XmlSerializerOutputFormatter) as XmlSerializerOutputFormatter;
+    public static XmlSerializerOutputFormatter? GetXmlSerializerOutputFormatter(
+        this MvcOptions mvcOptions
+    ) =>
+        mvcOptions.OutputFormatters.FirstOrDefault(
+            formatter => formatter is XmlSerializerOutputFormatter
+        ) as XmlSerializerOutputFormatter;
 
-    public static XmlDataContractSerializerInputFormatter? GetXmlDataContractSerializerInputFormatter(this MvcOptions mvcOptions)
-    => mvcOptions.InputFormatters.FirstOrDefault(formatter => formatter is XmlDataContractSerializerInputFormatter) as XmlDataContractSerializerInputFormatter;
+    public static XmlDataContractSerializerInputFormatter? GetXmlDataContractSerializerInputFormatter(
+        this MvcOptions mvcOptions
+    ) =>
+        mvcOptions.InputFormatters.FirstOrDefault(
+            formatter => formatter is XmlDataContractSerializerInputFormatter
+        ) as XmlDataContractSerializerInputFormatter;
 
-    public static XmlDataContractSerializerOutputFormatter? GetXmlDataContractSerializerOutputFormatter(this MvcOptions mvcOptions)
-    => mvcOptions.OutputFormatters.FirstOrDefault(formatter => formatter is XmlDataContractSerializerOutputFormatter) as XmlDataContractSerializerOutputFormatter;
-
+    public static XmlDataContractSerializerOutputFormatter? GetXmlDataContractSerializerOutputFormatter(
+        this MvcOptions mvcOptions
+    ) =>
+        mvcOptions.OutputFormatters.FirstOrDefault(
+            formatter => formatter is XmlDataContractSerializerOutputFormatter
+        ) as XmlDataContractSerializerOutputFormatter;
 
 #if NET8_0_OR_GREATER
     public static WebApplicationBuilder AddFormatters(this WebApplicationBuilder builder)
